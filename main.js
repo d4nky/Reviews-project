0f8b6083b1f379c45ef07dc05e33f01e6307ca3a
Fares Kherbane <kherbanefares@gmail.com>
2021-11-03 16:28:02 +0000

    Update main.js

--- main.js
+++ main.js
@@ -15,21 +15,21 @@ const reviews = [
     id: 1, name: 'Susan Smith', 
     job: 'WEB DEVELOPER',
     review: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    img: 'url(/Reviews/images/web_developer.jpg)',
+    img: 'url(/images/web_developer.jpg)',
 },
 
 {  
     id: 2, name: 'Anna Johnson', 
     job: 'WEB DESIGNER',
     review: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    img: 'url(/Reviews/images/web_designer.jpg)',
+    img: 'url(/images/web_designer.jpg)',
 },
 
 {   
     id: 3, name: 'Peter Jones', 
     job: 'INTERN',
     review: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    img: 'url(/Reviews/images/intern.jpg)',
+    img: 'url(/images/intern.jpg)',
 },
 
 {   
@@ -37,7 +37,7 @@ const reviews = [
     name: 'Bill Anderson', 
     job: 'THE BOSS',
     review:"Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
-    img: 'url(/Reviews/images/theboss.jpg)',
+    img: 'url(/images/theboss.jpg)',
 },
 ];
 
